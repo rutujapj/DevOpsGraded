15cf82ff31e30b05abff97e3ff865826476c5f14
Rutujajujare11 <jujarerutuja.prakash@hcl.com>
2021-11-23 20:47:53 +0530

    Submission

---
+++ helloworldapp/src/main/java/com/sts/HelloworldappApplication.java
@@ -0,0 +1,14 @@
+package com.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldappApplication.class, args);
+		System.out.println("Hello DevOps");
+	}
+
+}
